53223b498636b6f61b3286d2d322d21b92bb61ac
YeongKing <whdcks208@naver.com>
2024-03-15 23:57:47 +0900

    🔨AdminDeptMgtRegDesign의 액션기능 구현 클래스 AdminDeptMgtRegEvent 추가

--- src/eduCourse_prj/admin/AdminDeptMgtRegEvent.java
+++ src/eduCourse_prj/admin/AdminDeptMgtRegEvent.java
@@ -4,13 +4,45 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 
-public class AdminDeptMgtRegEvent extends WindowAdapter implements ActionListener {
+import javax.swing.JOptionPane;
 
+import eduCourse_prj.VO.CourseVO;
+
+public class AdminDeptMgtRegEvent extends WindowAdapter implements ActionListener {
+	AdminDeptMgtRegDesign admrd;
+	
+	public AdminDeptMgtRegEvent(AdminDeptMgtRegDesign admrd) {
+		this.admrd = admrd;
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent ae) {
+		
+		if(ae.getSource() == admrd.getRegisterButton()) {
+			JOptionPane.showMessageDialog(admrd, "등록버튼 클릭");
+            String departmentName = admrd.getDepartmentNameTextField().getText();
+            int capacity = Integer.parseInt(admrd.getDepartmentCapacityTextField().getText());
+            CourseVO cVO = new CourseVO(departmentName, capacity);
+//            acd.addDepartment(cVO);
+			
+			
+		}
+		if(ae.getSource()==admrd.getCancelButton()) {
+			
+			JOptionPane.showMessageDialog(admrd, "취소버튼 클릭");
+		}
+		
+		
 
 	}
 
