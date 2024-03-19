d1fab10d3a600532bd9a19a3d880bcc55e83fc7f
aoi562227 <aoi562227@gmail.com>
2024-03-19 17:52:43 +0900

    접고싶다

---
+++ src/eduCourse_prj/professor/event/ProfTestMdfyEvent.java
@@ -0,0 +1,94 @@
+package eduCourse_prj.professor.event;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+import eduCourse_prj.VO.DeptVO;
+import eduCourse_prj.VO.TestQustVO;
+import eduCourse_prj.admin.dao.AdminDAO;
+import eduCourse_prj.admin.design.AdminDeptMgtRegDesign;
+import eduCourse_prj.professor.dao.TestDAO;
+import eduCourse_prj.professor.design.ProfTestMdfyDesign;
+import eduCourse_prj.professor.design.ProfTestRegDesign;
+
+public class ProfTestMdfyEvent extends WindowAdapter implements ActionListener {
+	ProfTestMdfyDesign ptmd;
+
+	public ProfTestMdfyEvent(ProfTestMdfyDesign ptmd) {
+		this.ptmd = ptmd;
+
+	}
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		// TODO Auto-generated method stub
+		if (ae.getSource() == ptmd.getRegisterButton()) {
+			TestDAO tDAO = TestDAO.getInstance();
+			JOptionPane.showMessageDialog(ptmd, "출제버튼 클릭");
+			TestQustVO tqVO;
+			
+			int questionNumber = Integer.parseInt(ptmd.getTestNumberComboBox().getSelectedItem().toString());
+			System.out.println(questionNumber);
+			
+			String multipleChoiceOne = ptmd.getMultipleChoiceOneTextField().getText();
+			if(multipleChoiceOne.isEmpty()) {
+			    JOptionPane.showMessageDialog(ptmd, "선택(1)은 필수 입력 사항입니다.");
+			    return;
+			}
+			String multipleChoiceTwo = ptmd.getMultipleChoiceTwoTextField().getText();
+			if(multipleChoiceTwo.isEmpty()) {
+				JOptionPane.showMessageDialog(ptmd, "선택(2)은 필수 입력 사항입니다.");
+				return;
+			}
+			String multipleChoiceThree = ptmd.getMultipleChoiceThreeTextField().getText();
+			if(multipleChoiceThree.isEmpty()) {
+				JOptionPane.showMessageDialog(ptmd, "선택(3)은 필수 입력 사항입니다.");
+				return;
+			}
+			String multipleChoiceFour = ptmd.getMultipleChoiceFourTextField().getText();
+			if(multipleChoiceFour.isEmpty()) {
+				JOptionPane.showMessageDialog(ptmd, "선택(4)은 필수 입력 사항입니다.");
+				return;
+			}
+			
+			
+
+			String answerText = ptmd.getAnswerTextField().getText();
+			if(answerText.isEmpty()) {
+			    JOptionPane.showMessageDialog(ptmd, "정답은 필수 입력 사항입니다.");
+			    return;
+			}
+
+			try {
+			    int answer = Integer.parseInt(answerText);
+			    //capacity 유효성 검사
+			    if (answer <= 0 || answer >4) {
+			        JOptionPane.showMessageDialog(ptmd, "정답은 (1~4)만 입력 가능합니다.");
+			        return;
+			    }
+			    
+			    
+			    tqVO = new TestQustVO(questionNumber, "내용", answer,100403140, "M0001");
+			    tDAO.insertTest(tqVO);
+			}catch (SQLException se) {
+				JOptionPane.showMessageDialog(ptmd, "SQL예외가 발생했습니다.");
+				se.printStackTrace();
+			}
+
+//			System.out.println(tDAO.selectOneDept());
+
+		}
+		if (ae.getSource() == ptmd.getCancelButton()) {
+
+			JOptionPane.showMessageDialog(ptmd, "취소버튼 클릭");
+			ptmd.dispose();
+		}
+
+	}
+	
+
+}
+
