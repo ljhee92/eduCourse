0e0242bc7e512337591d39e85ed381272ebbe8c8
dbsdndcks <ywcsuwon@naver.com>
2024-03-22 17:41:46 +0900

    학사일정관리 저장 및 클릭 시 조회 구현
    학사일정관리 저장 및 클릭 시 조회 구현

--- src/eduCourse_prj/admin/dao/AdminAcadCalDAO.java
+++ src/eduCourse_prj/admin/dao/AdminAcadCalDAO.java
@@ -2,7 +2,10 @@ package eduCourse_prj.admin.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
 
 import eduCourse_prj.DbConnection;
 import eduCourse_prj.VO.AdminAcadCalVO;
@@ -21,12 +24,13 @@ public class AdminAcadCalDAO {
 		return aacDAO;
 	}//getInstance
 	
-	public String selectOneCal(String dayMonthYear) throws SQLException{
+	public String selectOneCal(String yearMonthDay) throws SQLException{
 		DbConnection dbCon = DbConnection.getInstance();
 		AdminAcadCalVO aacVO = null;
 		Connection con = null;
 		PreparedStatement pstmt = null;
-		
+		ResultSet rs = null ;
+		String memo = "";
 		try {
 			aacVO = new AdminAcadCalVO();
 			String id = "scott";
@@ -34,31 +38,33 @@ public class AdminAcadCalDAO {
 			
 			con = dbCon.getConnection(id, pass);
 			
-			String selectMemoCal = "select schedule_memo from schedule where schedule_input_date=(?)"
-					+ " ";
+			String selectMemoCal = "select schedule_memo from schedule where schedule_input_date=? ";
 			pstmt = con.prepareStatement(selectMemoCal);
 			
-			pstmt.setString(1, aacVO.getYearMonthDay());
-			pstmt.executeUpdate();
-			
+			pstmt.setString(1, yearMonthDay);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				memo = rs.getString("schedule_memo");	
+				System.out.println(memo);
+			}
 			
 
 		} finally {
 
-			dbCon.dbClose(null, pstmt, con);
+			dbCon.dbClose(rs, pstmt, con);
 
 		} // end finally
-		return dayMonthYear; ///////////////수정생각해야됨
+		return memo; ///////////////수정생각해야됨
 	}
 	
-	public void saveCal(String memo,String dayMonthYear) throws SQLException {
+	public void saveCal(String memo,String yearMonthDay) throws SQLException {
 		DbConnection dbCon = DbConnection.getInstance();
 		AdminAcadCalVO aacVO = null;
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		
 		try {
-			aacVO = new AdminAcadCalVO(memo, dayMonthYear);
+			aacVO = new AdminAcadCalVO(memo, yearMonthDay);
 			String id = "scott";
 			String pass = "tiger";
 			
@@ -78,7 +84,38 @@ public class AdminAcadCalDAO {
 		} // end finally
 	}
 	
-	public void deleteCal(String dayMonthYear) {
+	/**
+	 * 메모장을 업데이트하는 메서드
+	 * @param yearMonthDay
+	 * @throws SQLException 
+	 */
+	public void updateCal(String memo ,String yearMonthDay) throws SQLException {
+		DbConnection dbCon = DbConnection.getInstance();
+		AdminAcadCalVO aacVO = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			aacVO = new AdminAcadCalVO(memo, yearMonthDay);
+			String id = "scott";
+			String pass = "tiger";
+			
+			con = dbCon.getConnection(id, pass);
+			
+			String saveCal = "	update schedule "
+					+ "	set	schedule_memo = ? "
+					+ "	where schedule_input_date = ? ";
+			pstmt = con.prepareStatement(saveCal);			
+			pstmt.setString(1, memo);
+			pstmt.setString(2, yearMonthDay);
+			pstmt.executeUpdate();
+			
+		} finally {
+			dbCon.dbClose(null, pstmt, con);
+		} // end finally
+	}
+	
+	public void deleteCal(String yearMonthDay) {
 		
 	}
 	
