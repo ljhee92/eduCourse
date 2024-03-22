6cf4f2cee32b9324a8890ee3a609f595f159a9e3
dbsdndcks <ywcsuwon@naver.com>
2024-03-22 16:10:26 +0900

    시험출제여부따른 라디오버튼 수정
    시험출제여부따른 라디오버튼 수정

--- src/eduCourse_prj/professor/event/ProfTestMgtEvent.java
+++ src/eduCourse_prj/professor/event/ProfTestMgtEvent.java
@@ -32,17 +32,32 @@ public class ProfTestMgtEvent extends WindowAdapter implements ActionListener, L
 		
 		if(ae.getSource() == ptmd.getJbtnTestReg()) {
 			JOptionPane.showMessageDialog(ptmd, "등록버튼 클릭");
+			int seletedRow = ptmd.getJtbTestMgt().getSelectedRow();
+			String seletedValue = (String) ptmd.getJtbTestMgt().getValueAt(seletedRow,2);
+			if(seletedValue.equals("출제완료")) {
+				JOptionPane.showMessageDialog(ptmd,"이미 출제 완료된 시험입니다\n     <수정가능>");
+				return;
+			}
 			new ProfTestRegDesign(ptmd, null);
 		}
 		//////////////////////////활성화 버튼 클릭시///////////////////////////
 		if(ae.getSource() == ptmd.getJrbtnEnable()) {
 			JOptionPane.showMessageDialog(ptmd, "활성화 버튼 클릭");
 			int index = ptmd.getJtbTestMgt().getSelectedRow();
+			
 			if (index == -1) {
 				JOptionPane.showMessageDialog(ptmd, "과목을 선택해주세요.");
 				return;
 			} // end if
+			
 			String course_name = ptmd.getDtmTestMgt().getValueAt(index, 0).toString();
+			String testEnable = ptmd.getDtmTestMgt().getValueAt(index, 2).toString();			
+			if(!testEnable.equals("출제완료")) {
+				JOptionPane.showMessageDialog(ptmd, "시험 활성화는 출제완료된 과목만 가능합니다");
+				ptmd.getJrbtnDisable().setSelected(true);
+				return;
+			}
+			
 			
 			ProfDAO pDAO = ProfDAO.getInstance();
 			try {
@@ -90,7 +105,6 @@ public class ProfTestMgtEvent extends WindowAdapter implements ActionListener, L
 	        if (!event.getValueIsAdjusting()) {
 	            int selectedRow = ptmd.getJtbTestMgt().getSelectedRow();
 	            if (selectedRow != -1) {
-	                JOptionPane.showMessageDialog(null, "테이블의 행이 클릭되었습니다." );
 	                String check = (String) ptmd.getJtbTestMgt().getValueAt(selectedRow, 1);
 	                
 	                if (check.equals("Y")) {
